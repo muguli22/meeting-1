ce748d282d346bef558c57d7bde5954daf525dfc
root <root@mail.toiminnot.com>
2017-05-06 12:58:54 +0000

    First commit

---
+++ meeting/meeting/doctype/meeting_agenda/meeting_agenda.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Erno Viitanen and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Meeting Agenda', {
+	refresh: function(frm) {
+
+	}
+});
